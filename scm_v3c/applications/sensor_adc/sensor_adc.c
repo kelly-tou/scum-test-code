979d6c68ee8eb840bf9999b59fcaf0ce54a424a3
Kelly Tou <kellytou@berkeley.edu>
2024-07-15 18:16:08 -0700

    remove changes from testing

--- scm_v3c/applications/sensor_adc/sensor_adc.c
+++ scm_v3c/applications/sensor_adc/sensor_adc.c
@@ -4,19 +4,13 @@
 #include <stdlib.h>
 
 #include "adc.h"
-#include "fixed_point.h"
-#include "matrix.h"
 #include "memory_map.h"
 #include "optical.h"
 #include "scm3c_hw_interface.h"
-#include "svd.h"
 
 // Number of for loop cycles between ADC reads.
 // 700000 for loop cycles roughly correspond to 1 second.
 #define NUM_CYCLES_BETWEEN_ADC_READS 700000
-
-matrix_t g_matrix, v;
-fixed_point_short_t a, b, c, d;
 
 // ADC configuration.
 static const adc_config_t g_adc_config = {
@@ -48,41 +42,9 @@ int main(void) {
     perform_calibration();
 
     while (true) {
-        // printf("Reading the ADC output.\n");
-        // uint16_t adc_output = adc_read_output();
-        // printf("ADC output: %u\n", adc_output);
-
-        matrix_init(&g_matrix, 2, 2);
-        matrix_init(&v, 2, 2);
-        matrix_set(&g_matrix, 0, 0, fixed_point_init(4));   // a
-        matrix_set(&g_matrix, 0, 1, fixed_point_init(0));   // b
-        matrix_set(&g_matrix, 1, 0, fixed_point_init(3));   // c
-        matrix_set(&g_matrix, 1, 1, fixed_point_init(-5));  // d
-
-        // matrix_set(&g_matrix, 0, 0, fixed_point_init(1)); // a
-        // matrix_set(&g_matrix, 0, 1, fixed_point_init(0)); // b
-        // matrix_set(&g_matrix, 1, 0, fixed_point_init(0)); // c
-        // matrix_set(&g_matrix, 1, 1, fixed_point_init(1)); // d
-
-        svd_init(&g_matrix);
-        svd_calculate_v(&v);
-
-        matrix_get(&v, 0, 0, &a);
-        matrix_get(&v, 0, 1, &b);
-        matrix_get(&v, 1, 0, &c);
-        matrix_get(&v, 1, 1, &d);
-
-        // matrix_get(&g_matrix, 0, 0, &a);
-        // matrix_get(&g_matrix, 0, 1, &b);
-        // matrix_get(&g_matrix, 1, 0, &c);
-        // matrix_get(&g_matrix, 1, 1, &d);
-
-        // a = fixed_point_init(4);
-        // b = fixed_point_init(0);
-        // c = fixed_point_init(3);
-        // d = fixed_point_init(-5);
-
-        printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+        printf("Reading the ADC output.\n");
+        uint16_t adc_output = adc_read_output();
+        printf("ADC output: %u\n", adc_output);
 
         // Wait for the next ADC read.
         for (size_t i = 0; i < NUM_CYCLES_BETWEEN_ADC_READS; ++i) {}
